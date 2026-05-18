2775b25ca8bf290f207415239190aae17e4b84ce
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-18 14:48:41 +0700

    menambahkan fungsi getSaklarNo untuk mengakses nilai saklar

--- Enskapsulasi.cpp
+++ Enskapsulasi.cpp
@@ -9,3 +9,10 @@ public:
     void setSaklarNo(int i, string value) {
         saklarNo[i] = value;
     }
+
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+int main() {
+    remoteLampu lampuRumah
