165d1628ef29e23a6f3ad52e239d15fd3bf584a3
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-18 14:30:33 +0700

    membuat class orang

---
+++ SolveMulitwaris.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+class orang {
+public:
+int umur;
+orang(int Pumur) 
+    umur(Pumur)
+    { cout << "orang dibuat dengan umur " << umur << endl; }
+};
+
+
