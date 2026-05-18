34e00f7bcefa6e9073763c7bc19619d78cabd6f5
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-18 14:49:23 +0700

    menambahkan inisialisasi saklar dan output nilai saklar

--- Enskapsulasi.cpp
+++ Enskapsulasi.cpp
@@ -15,4 +15,18 @@ public:
     }
 };
 int main() {
-    remoteLampu lampuRumah
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+    lampuRumah.setSaklarNo(4, "Lampu WC");
+
+    cout << "Saklar 1: " << lampuRumah.getSaklarNo(0) << endl;
+    cout << "Saklar 2: " << lampuRumah.getSaklarNo(1) << endl;
+    cout << "Saklar 3: " << lampuRumah.getSaklarNo(2) << endl;
+    cout << "Saklar 4: " << lampuRumah.getSaklarNo(3) << endl;
+    cout << "Saklar 5: " << lampuRumah.getSaklarNo(4) << endl;
+
+    return 0;
+}
