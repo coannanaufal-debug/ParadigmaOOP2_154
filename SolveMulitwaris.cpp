f22421ab058b778686cf11707f7c4d31b8cd31d4
coannanaufal-debug <coanna.naufal@gmail.com>
2026-05-18 14:33:04 +0700

    menambahkan class budi yang mewarisi class pekerja dan pelajar

--- SolveMulitwaris.cpp
+++ SolveMulitwaris.cpp
@@ -27,3 +27,14 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+
